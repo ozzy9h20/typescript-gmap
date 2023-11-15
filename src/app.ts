bd667bef819eababda0960fd46df06f84b7fe5ea
Mochammad Endyarroziqin <rufiozzie@gmail.com>
2023-11-15 11:48:47 +0700

    Section 13 - Chapter 173 ~ Rendering a Map with Google Maps

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import axios from 'axios'
+import { Loader } from '@googlemaps/js-api-loader'
 
-const API_KEY = process.env.GOOGLE_MAP_API_KEY
+const API_KEY = process.env.GOOGLE_MAP_API_KEY!
 const API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+
+const loader = new Loader({
+  apiKey: API_KEY, 
+  version: 'weekly'
+})
 
 const form = document.querySelector('form')! as HTMLFormElement
 const addressInput = document.getElementById('address')! as HTMLInputElement
@@ -13,12 +19,20 @@ function searchAddressHandler(event: Event) {
   
   axios
     .get<GoogleGeocodingResponse>(`${API_URL}?address=${encodeURI(enteredAddress)}&key=${API_KEY}`)
-    .then((res) => {
+    .then(async (res) => {
       if (res.data.status !== 'OK') {
         throw new Error('Could not fetch location!')
       }
       const coordinates = res.data.results[0].geometry.location
-      console.log("🚀 ~ file: app.ts:21 ~ .then ~ coordinates:", coordinates)
+      
+      await loader.load()
+      const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+      const map = new Map(document.getElementById("map") as HTMLElement, {
+        center : coordinates,
+        zoom   : 14,
+      })
+
+      new google.maps.Marker({ position: coordinates, map })
     })
     .catch((err) => {
       alert(err.message)
